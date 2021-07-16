711ad3f9a517990d90342f5f51530d18d38bb828
weissle <weissle9801@gmail.com>
2021-07-16 17:43:34 +0800

    amalgamate to one hpp

--- tool/amalgamate_to_one.sh
+++ tool/amalgamate_to_one.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-mkdir ../single_include -p
 cp ./amalgamate.txt ../include/.
 cp ./amalgamate/amalgamate.py ../include/.
 cd ../include
-python3 amalgamate.py amalgamate.txt ../single_include/json.hpp
+python3 amalgamate.py amalgamate.txt ../json.hpp
 rm amalgamate.py
 rm amalgamate.txt
